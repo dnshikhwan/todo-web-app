0849ae75f727a62a801eec25acdb175c1a3c58cb
danish ikhwan <53143159+Zealogy@users.noreply.github.com>
2020-04-05 23:51:36 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router , Route , Link} from 'react-router-dom'
+import Todolist from './component/todo-list.component'
+import CreateTodo from './component/create-list.component'
+import EditTodo from './component/edit-list.component'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <Link to="/" className="navbar-brand">Dlab</Link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/create" className="nav-link">CreateTodo</Link>
+              </li>
+            </ul>
+          </div>
+     </div>
+      </nav>
+      </div>
+
+    <div className="container">
+        <Route path="/" exact component={Todolist} />
+        <Route path="/create" component={CreateTodo} />
+        <Route path="/update/:id" component={EditTodo} />
     </div>
+    </Router>
   );
 }
 
